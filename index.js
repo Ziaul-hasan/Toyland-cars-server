c720f13397f7530da174684fbb4e51604d736717
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-05-19 16:31:43 +0600

    get data by category set

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/toys/:text', async(req, res) => {
+      console.log(req.params.text)
+      const result = await toyCollection.find({subcategory: req.params.text}).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
